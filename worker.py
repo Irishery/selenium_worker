6f6570d3ad810d835705fef8500e09599406bb2d
Irishey <markirill21@gmail.com>
2022-05-11 00:09:35 +0200

    migrate to another mail

--- worker.py
+++ worker.py
@@ -1,6 +1,7 @@
 import os
 import random
 from time import sleep
+import httpx
 from selenium.webdriver.common.by import By
 from selenium.common.exceptions import TimeoutException
 from selenium.webdriver.support.ui import WebDriverWait
@@ -30,7 +31,9 @@ class Worker:
         options.binary_location = '/home/kir/chromes/google-chrome-beta_current_x86_64/opt/google/chrome-beta/google-chrome-beta'
         # options.headless = True
         self.driver = uc.Chrome(options=options)
-    
+        self.driver.set_window_size(1920, 1080)
+        self.client = httpx.Client(timeout=None)
+
     def main_runner(self):
         self.log_in()
         # self.approve_new_device()
@@ -61,12 +64,14 @@ class Worker:
 
         log_in = self.driver.find_element(By.CSS_SELECTOR, '#new_user > div > div:nth-child(5) > input')
         log_in.click()
-
+        sleep
 
         if self.check_recaptcha():
             print('CAPTCHA')
-            sleep(20)
-
+            while self.check_recaptcha():
+                sleep(10)
+        print('end captcha')
+        sleep(20)
         self.approve_new_device()
     
     def human_type(self, element, text) -> None:
@@ -77,11 +82,19 @@ class Worker:
     def check_recaptcha(self) -> bool: 
         delay = 3
         try:
-            captcha = WebDriverWait(self.driver, delay).until(EC.presence_of_element_located((By.ID, 'freshworks-frame')))
+            captcha = WebDriverWait(self.driver, delay).until(EC.presence_of_element_located((By.CLASS_NAME, 'challenge-modal')))
+            print(captcha.is_displayed())
             return True
         except Exception as e:
             print(e)
             return False
+    
+    def ping(self, data):
+        r = self.client.request(
+            "get", "http://127.0.0.1:5000/api/selenium/post_data",
+            params={'data_from_worker': data},
+            headers={'content-type': 'application/json'}
+        )
     
     def approve_new_device(self):
         while True:
@@ -92,71 +105,121 @@ class Worker:
             except IndexError:
                 continue
 
-        self.driver.get('https://protonmail.com/')
-        input()
+        self.driver.get('https://e.mail.ru/')
 
-        log_in = self.driver.find_element(By.XPATH, '/html/body/div[1]/nav/div[3]/div/div[2]/ul/li[8]/a')
-        log_in.click()
-        input()
+        # log_in = self.driver.find_element(By.XPATH, '/html/body/div[1]/nav/div[3]/div/div[2]/ul/li[8]/a')
+        # log_in.click()
 
         delay = 10 # seconds
-        try:
-            email = WebDriverWait(self.driver, delay).until(EC.presence_of_element_located((By.ID, 'username')))
-            print("Page is ready!")
-        except TimeoutException:
-            print("Loading took too much time!")
+        while True:
+            try:
+                email = WebDriverWait(self.driver, delay).until(EC.presence_of_element_located((By.XPATH, '/html/body/div[1]/div[2]/div/div[2]/div/div/div/div/form/div[2]/div[2]/div[1]/div/div/div/div/div/div[1]/div/input')))
+                break
+            except TimeoutException:
+                print("Page is loading")
 
         email.click()
+        while True:
+            try:
+                self.human_type(email, gmail_login)
+                break
+            except Exception as e:
+                print(e)
 
-        self.human_type(email, gmail_login)
+        go_to_pass = self.driver.find_element(By.XPATH, '/html/body/div[1]/div[2]/div/div[2]/div/div/div/div/form/div[2]/div[2]/div[3]/div/div/div[1]/button')
+        go_to_pass.click()
+        while True:
+            try:
+                password = WebDriverWait(self.driver, delay).until(EC.presence_of_element_located((By.XPATH, '/html/body/div[1]/div[2]/div/div[2]/div/div/div/div/form/div[2]/div/div[2]/div/div/div/div/div/input')))
+                break
+            except TimeoutException:
+                print("Page is loading")
 
-        # go_to_pass = self.driver.find_element(By.CSS_SELECTOR, '#identifierNext > div > button')
-        # go_to_pass.click()
+        while True:
+            try:
+                self.human_type(password, gmail_pass)
+                break
+            except Exception as e:
+                print(e)
+        sleep(3)
 
-        try:
-            password = WebDriverWait(self.driver, delay).until(EC.presence_of_element_located((By.ID, 'password')))
-            print("Page is ready!")
-        except TimeoutException:
-            print("Loading took too much time!")
-
-        self.human_type(password, gmail_pass)
-        input()
-
-        enter = self.driver.find_element(By.XPATH, '/html/body/div[1]/div[3]/div/div/main/div[2]/form/button')
+        enter = self.driver.find_element(By.XPATH, '/html/body/div[1]/div[2]/div/div[2]/div/div/div/div/form/div[2]/div/div[3]/div/div/div[1]/div/button')
         enter.click()
 
         while True:
             try:
-                search = self.driver.find_element(By.XPATH, '/html/body/div[1]/div[3]/div[3]/header/div[2]/div')
-                print("Page is ready!")
+                search = self.driver.find_element(By.XPATH, '/html/body/div[5]/div/div[1]/div[1]/div/div[1]/span/div[4]/div/div[1]/div')
                 break
             except Exception as e:
-                print("Loading took too much time!")
+                print("Page is loading")
         
         search.click()
-
-        search_input = self.driver.find_element(By.ID, 'search-keyword')
-        input()
-
-
-        self.human_type(search_input, 'coinlist')
-        input()
-        search_btn = self.driver.find_element(By.XPATH, '/html/body/div[4]/div[2]/form/div[3]/button[2]')
-        search_btn.click()
-        input()
+        sleep(3)
 
         while True:
             try:
-                approve_letter = self.driver.find_element(By.XPATH, '/html/body/div[1]/div[3]/div[3]/div/div[2]/div/main/div/div/div/div[2]')
-                print("Page is ready!")
+                search_input = self.driver.find_element(By.XPATH, '/html/body/div[5]/div/div[1]/div[1]/div/div[1]/span/div[4]/div/div[3]/div/div/span/span/div/input')
                 break
             except Exception as e:
-                print("Loading took too much time!")
+                search.click()
+                print('Page is loading')
+        
+        sleep(3)
+
+        while True:
+            try:
+                self.human_type(search_input, 'coinlist')
+                break
+            except Exception as e:
+                print('Page is loading')
+        sleep(3)
+
+        while True:
+            try:
+                search_btn = self.driver.find_element(By.XPATH, '/html/body/div[5]/div/div[1]/div[1]/div/div[1]/span/div[4]/div/div[3]/div/span[2]')
+                break
+            except Exception as e:
+                print('Page is loading')
+        search_btn.click()
+
+        while True:
+            try:
+                approve_letter = self.driver.find_element(By.XPATH, '/html/body/div[5]/div/div[1]/div[1]/div/div[2]/span/div[2]/div/div/div/div/div[1]/div/div/div/div[1]/div/div/a[1]/div[4]/div/div[1]')
+                break
+            except Exception as e:
+                print("Page is loading")
 
         approve_letter.click()
-        input()
 
-        approve_link = self.driver.find_element(By.CSS_SELECTOR, '#body-wrapper > tbody > tr > td > div > table:nth-child(2) > tbody > tr > td > div.layouts-coinlist_mailer-application__content.layouts-coinlist_mailer-application__content--normal > div > div.s-paddingTop1.u-text-center.s-paddingBottom2 > a')
+        while True:
+            try:
+                approve_link = self.driver.find_element(By.XPATH, '/html/body/div[5]/div/div[1]/div[1]/div/div[2]/span/div[2]/div/div/div/div/div/div/div[2]/div[1]/div[3]/div[2]/div/div/div/div/div/div/div/div/div/div/table/tbody/tr/td/div/table[1]/tbody/tr/td/div[2]/div/div[7]/a')
+                break
+            except Exception as e:
+                print('Page is loading')
         approve_link.click()
-        input()
+
+        sleep(15)
+        
+        self.ping('pong')
+
+        # while True:
+        #     try:
+        #         account_btn = self.driver.find_element(By.CSS_SELECTOR, 'body > div.u-height100.layouts-shared-market > div > div.js-sidebar.layouts-shared-market__sidebar > div > div.layouts-shared-market-sidebar__open > div.u-displayInlineBlock.layouts-shared-market-sidebar__icon > a')
+        #         break
+        #     except Exception as e:
+        #         print(e)
+        
+        # account_btn.click()
+        
+
+        # while True:
+        #     try:
+        #         personal_info = self.driver.find_element(By.XPATH, '/html/body/div[2]/div/div[2]/div[2]/div[1]/div/div/div/div[2]/a')
+        #         break
+        #     except Exception as e:
+        #         print(e)
+
+        # personal_info.click()
+
         # input()
